7de53137ea084dd6a447a8273a7bc43a2ee1fe6e
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-09-08 12:40:20 +0600

    style(native): align execution failure enum

--- src/Native/NativeExecutionFailure.php
+++ src/Native/NativeExecutionFailure.php
@@ -7,10 +7,16 @@ namespace Infocyph\Pathwise\Native;
 enum NativeExecutionFailure: string
 {
     case EXIT_CODE = 'exit_code';
+
     case IO_ERROR = 'io_error';
+
     case START_FAILED = 'start_failed';
+
     case STDERR_LIMIT = 'stderr_limit';
+
     case STDOUT_LIMIT = 'stdout_limit';
+
     case TIMEOUT = 'timeout';
+
     case UNSUPPORTED = 'unsupported';
 }
